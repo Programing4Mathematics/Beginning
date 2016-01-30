63cc8db7d4ae282949da7a53b27a5b0a5d6531f5
Minesh Jethva <minesh.1291@gmail.com>
2016-01-31 00:13:17 +0530

    Create Circles.py

---
+++ Geometry/Circles.py
@@ -0,0 +1,12 @@
+>>> C1 = Circle(P1, 3)
+>>> C2 = Circle(P2, P3, P4)
+>>> C2.area
+1105*pi/98
+>>> C2.radius
+sqrt(2210)/14
+>>> C2.equation()
+(x - 5/14)**2 + (y - 27/14)**2 - 1105/98
+>>> C2.center
+Point(5/14, 27/14)
+>>> C2.circumference
+sqrt(2210)*pi/7
