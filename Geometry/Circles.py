e86fc03ef58f7ae9f15aa3a1c4eb3575447d929a
Minesh Jethva <minesh.1291@gmail.com>
2016-01-31 00:15:10 +0530

    Update Circles.py

--- Geometry/Circles.py
+++ Geometry/Circles.py
@@ -10,3 +10,14 @@ sqrt(2210)/14
 Point(5/14, 27/14)
 >>> C2.circumference
 sqrt(2210)*pi/7
+
+>>> C2.intersection(C1)
+[Point(55/754 + 27*sqrt(6665)/754, -5*sqrt(6665)/754 + 297/754),
+ Point(-27*sqrt(6665)/754 + 55/754, 297/754 + 5*sqrt(6665)/754)]
+>>> C2.intersection(S1)
+[Point(3, 4)]
+>>> C2.is_tangent(L2)
+False
+>>> C1.tangent_lines(P4)
+[Line(Point(-1, 5), Point(-9/26 + 15*sqrt(17)/26, 3*sqrt(17)/26 + 45/26)),
+ Line(Point(-1, 5), Point(-15*sqrt(17)/26 - 9/26, -3*sqrt(17)/26 + 45/26))]
